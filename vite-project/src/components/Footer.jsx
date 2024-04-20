2df0ea899e589ea9aefd3f15f83f2a2c9a17e99e
Damla Sabaz <damla.sabaz@hotmail.com>
2024-04-20 23:23:42 +0300

    Update CSS, Footer and Font

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -14,7 +14,8 @@ export const Footer = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col size={12} sm={6}>
-                        <img src={logo} alt="Logo" />
+                         <p>Copyright 2024. All Rights Reserved</p>
+                        <p>Damla Sabaz & Mehmet Batuhan Yazıcı</p>
                     </Col>
                     <Col size={12} sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
@@ -24,8 +25,7 @@ export const Footer = () => {
                             
                         </div>
                         
-                        <p>Copyright 2024. All Rights Reserved</p>
-                        <p>Damla Sabaz & Mehmet Batuhan Yazıcı</p>
+                       
                     </Col>
                 </Row>
             </Container>
