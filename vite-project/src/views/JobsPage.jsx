3ef0c5d36b44e16e776f638fae87b5e49c976d8a
Damla Sabaz <damla.sabaz@hotmail.com>
2024-04-20 22:51:41 +0300

    Update UI

--- vite-project/src/views/JobsPage.jsx
+++ vite-project/src/views/JobsPage.jsx
@@ -1,10 +1,14 @@
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Createjob from '../components/CreateJob';
-
+import { Helmet } from 'react-helmet-async';
 function JobsPage() {
   return (
     <>
+      <Helmet>
+        <title>Add Jobs</title>
+        <meta name="description" content="Adding Jobs" />
+      </Helmet>
       <NavBar />
       <Createjob />
       <Footer />
--- vite-project/src/views/MatchPage.jsx
+++ vite-project/src/views/MatchPage.jsx
@@ -1,11 +1,15 @@
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 import MatchOperation from '../components/MatchOperation'
-
+import { Helmet } from 'react-helmet-async';
 
 function MatchPage() {
   return (
     <>
+      <Helmet>
+        <title>Match Jobs</title>
+        <meta name="description" content="Adding Jobs" />
+      </Helmet>
       <NavBar />
       <MatchOperation />
       <Footer />
