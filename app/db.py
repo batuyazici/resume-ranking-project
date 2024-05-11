3336e55e8f77fc2655ce7d70a7429fda16b02946
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2024-05-11 12:53:41 +0300

    Refactor database connection pool initialization and closing

--- app/db.py
+++ app/db.py
@@ -1,22 +1,24 @@
 import asyncpg
 from config import DATABASE_URL
 
-async def create_connection_pool():
-    return await asyncpg.create_pool(DATABASE_URL)
+pool = None  # Global variable for the connection pool
+
+async def init_connection_pool():
+    global pool
+    pool = await asyncpg.create_pool(DATABASE_URL)
+    return pool
+
+async def close_connection_pool():
+    await pool.close()
 
 async def execute_query(query: str, *args, **kwargs):
-    async with await create_connection_pool() as pool:
-        async with pool.acquire() as connection:
-            return await connection.execute(query, *args, **kwargs)
+    async with pool.acquire() as connection:
+        return await connection.execute(query, *args, **kwargs)
 
 async def fetch_query(query: str, *args, **kwargs):
-    async with await create_connection_pool() as pool:
-        async with pool.acquire() as connection:
-            return await connection.fetch(query, *args, **kwargs)
+    async with pool.acquire() as connection:
+        return await connection.fetch(query, *args, **kwargs)
 
 async def fetch_single_query(query: str, *args, **kwargs):
-    async with await create_connection_pool() as pool:
-        async with pool.acquire() as connection:
-            # Use fetchval to get a single value result, such as an ID
-            return await connection.fetchval(query, *args, **kwargs)
-
+    async with pool.acquire() as connection:
+        return await connection.fetchval(query, *args, **kwargs)
